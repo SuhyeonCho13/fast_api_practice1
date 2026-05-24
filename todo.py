7dae76cc3c2de38c9de025f00e1c9497870cebbe
SuhyeonCho <lunaspdlqj0913@naver.com>
2026-05-24 10:12:48 +0900

    practice

--- todo.py
+++ todo.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Path
+from model import Todo
 
 todo_router = APIRouter()
 
 todo_list = []
 
 @todo_router.post("/todo")
-async def add_todo(todo: dict) -> dict:
+async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return{
         "msg" : "todo added successfully"
@@ -17,3 +17,10 @@ async def retrieve_todos() -> dict:
     return{
         "todos" : todo_list
     }
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(...,title = "the ID of the todo to retrieve")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo.id:
+            return { "todo" : todo}
+    return {"msg" :"todo with supplied IS doesn't exist"}
